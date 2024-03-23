8902c1788dcaa823b352a5a7ed58d7a0c8d9af10
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-03-23 16:59:05 +0600

    add dark and light toggle icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     const [innerWidth, setinnerWidth] = useState<number>(1025)
     const [showNav, setShowNav] = useState<boolean>(false)
+    const [isDark, setIsDark] = useState<boolean>(false)
 
     function handleNav() {
         setShowNav(prev => !prev)
+    }
+
+    function handleToggle() {
+        setIsDark(prev => !prev)
     }
 
     useEffect(() => {
@@ -46,6 +51,13 @@ const Navbar = () => {
                     <div className="right-section lg:w-1/2 flex gap-5 lg:flex-auto items-center">
                         <Link className="px-4 py-2 rounded-lg bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 text-white font-semibold" to="/">Buy Now</Link>
                         <svg onClick={handleNav} className="lg:hidden text-white feather feather-menu cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+
+                        <div>
+                            {
+                                isDark ? <svg onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white cursor-pointer feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg> :
+                                    <svg onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white cursor-pointer feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
