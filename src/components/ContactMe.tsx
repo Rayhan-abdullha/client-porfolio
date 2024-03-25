b35ac3aac7fe5a2ade82d97f1030efd2b2fdd266
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-03-25 13:37:15 +0600

    footer menulist hover css added

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -13,7 +13,7 @@ const ContactMe = () => {
                 <form className='md:w-1/2 flex flex-col gap-5 mt-10 md:mt-0'>
                     {
                         contactForm.map(item => (
-                            item.type === 'textarea' ? <textarea className={`rounded-xl border p-5 outline-none ${isDarkMode ? 'bg-[#060606] text-[#acacac] border border-[hsla(0,0%,100%,0.1)]' : 'border border-[#aaaaaa] bg-[#f5f8fa]'}`} name='text' placeholder='Enter your message'></textarea> : <input key={item.id} className={`rounded-xl border p-5 outline-none ${isDarkMode ? 'bg-[#060606] text-[#acacac] border border-[hsla(0,0%,100%,0.1)]' : 'border border-[#aaaaaa] bg-[#f5f8fa]'}`} type={item.type} placeholder={item.label} />
+                            item.type === 'textarea' ? <textarea className={`bg-transparent rounded-xl border p-5 outline-none ${isDarkMode ? 'bg-[#060606] text-[#acacac] border border-[hsla(0,0%,100%,0.1)]' : 'border border-[#aaaaaa] bg-[#f5f8fa]'}`} name='text' placeholder='Enter your message'></textarea> : <input key={item.id} className={`bg-transparent rounded-xl border p-5 outline-none ${isDarkMode ? 'bg-[#060606] text-[#acacac] border border-[hsla(0,0%,100%,0.1)]' : 'border border-[#aaaaaa] bg-[#f5f8fa]'}`} type={item.type} placeholder={item.label} />
                         ))
                     }
                     <button className='rounded-md py-3 px-5 font-medium text-white text-lg bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 w-[200px] transition duration-300 ease-in-out' type='submit'>Submit</button>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ import { useTheme } from "../context/ThemeContext"
 const Footer = () => {
     const { isDarkMode } = useTheme()
     return (
-        <div className={`flex py-10 border-t ${isDarkMode ? 'border-gray-500 bg-[#0F0F11]' : 'border-gray-300'}`}>
+        <div className={`flex py-7 border-t ${isDarkMode ? 'border-gray-500 bg-[#0F0F11]' : 'border-gray-300 bg-[#f5f8fa]'}`}>
             <div className="container mx-auto px-5 sm:px-12 flex flex-col gap-5 md:gap-2 md:flex-row items-center md:justify-between">
                 <ul className="flex gap-5">
                     {
                         footerInfo.menu.map((item: { id: number, item: string }) => (
-                            <li key={item.id}><Link className={`${isDarkMode ? 'text-[#acacac]' : 'text-[#6c6c6c]'} text-[#acacac] text-[16px] font-semibold`} to="/">{item.item}</Link></li>
+                            <li className="relative" key={item.id}><Link className={`hover-a relative ${isDarkMode ? 'text-[#acacac]' : 'text-[#65676b]'} hover:text-[#059dff] text-[15px] font-semibold transition-color duration-300 ease-in-out before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-[#059dff] before:transition-width before:duration-300 before:ease-in-out hover:before:w-full group-hover:before:w-full`} to="/">{item.item}</Link></li>
                         ))
                     }
                 </ul>
@@ -18,14 +18,14 @@ const Footer = () => {
                     <ul className="flex gap-5">
                         {
                             contactMe.socialLinks.map((item: { id: number, url: string }) => (
-                                <li key={item.id} className={`${isDarkMode ? 'bg-[#2e2e2e]' : 'bg-white'} hover:text-white rounded-full p-3 cursor-pointer hover:bg-[#1BA1F2] transition duration-300 ease-in-out`}>
+                                <li key={item.id} className={`${isDarkMode ? 'bg-[#2e2e2e]' : 'bg-white'} hover:text-white rounded-full p-3 cursor-pointer hover:bg-[#1BA1F2] transition-color duration-500 ease-in-out`}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`${isDarkMode ? 'text-white' : 'text-black'} w-[18px] rounded-full] feather feather-facebook`}><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
                                 </li>
                             ))
                         }
                     </ul>
                 </div>
-                <p className={`${isDarkMode ? 'text-[#acacac]' : 'text-[#65676b]'} text-[16px]`}>{footerInfo.copyright}</p></div>
+                <p className={`${isDarkMode ? 'text-[#acacac]' : 'text-[#65676b]'} text-[15px]`}>{footerInfo.copyright}</p></div>
         </div >
     )
 }
